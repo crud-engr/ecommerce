98338baea04c5d9ddbd129e10b8d3df998b78c99
Abeeb Ayinla <abeebayinla@gmail>
2021-11-29 12:35:59 +0100

    prepare for production

--- config/database.ts
+++ config/database.ts
@@ -24,40 +24,40 @@ const databaseConfig: DatabaseConfig = {
   */
   connection: Env.get('DB_CONNECTION'),
 
+//   connections: {
+//     host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+//     port: Env.get('DB_PORT', ''),
+//     user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+//     password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+//     database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+// }
+
   connections: {
-    host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-    port: Env.get('DB_PORT', ''),
-    user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-    password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-    database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
-}
+    /*
+    |--------------------------------------------------------------------------
+    | SQLite
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for the SQLite database.  Make sure to install the driver
+    | from npm when using this connection
+    |
+    | npm i sqlite3
+    |
+    */
+    sqlite: {
+      client: 'sqlite',
+      connection: {
+        filename: Application.tmpPath('db.sqlite3'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      useNullAsDefault: true,
+      healthCheck: false,
+      debug: false,
+    },
 
-  // connections: {
-  //   /*
-  //   |--------------------------------------------------------------------------
-  //   | SQLite
-  //   |--------------------------------------------------------------------------
-  //   |
-  //   | Configuration for the SQLite database.  Make sure to install the driver
-  //   | from npm when using this connection
-  //   |
-  //   | npm i sqlite3
-  //   |
-  //   */
-  //   sqlite: {
-  //     client: 'sqlite',
-  //     connection: {
-  //       filename: Application.tmpPath('db.sqlite3'),
-  //     },
-  //     migrations: {
-  //       naturalSort: true,
-  //     },
-  //     useNullAsDefault: true,
-  //     healthCheck: false,
-  //     debug: false,
-  //   },
-
-  // }
+  }
 }
 
 export default databaseConfig
